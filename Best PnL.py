40a4a725d67454913ad62595e1a6be5815419fae
Matthew Veld <veldm777@gmail.com>
2026-04-13 13:52:34 +1000

    Added linear edge, skew

--- Best PnL.py
+++ Best PnL.py
@@ -1,68 +1,110 @@
-from datamodel import OrderDepth, UserId, TradingState, Order
+from datamodel import OrderDepth, UserId, TradingState, Order, Symbol
 from typing import List
+from numpy import mean, sign
 
+THEO_EMERALDS = 10000
+POSITION_MAX = {'TOMATOES': 80, 'EMERALDS': 80}
+SKEW_MAX = 1
+EDGE = 1
+
+class Product:
+    """Product container for trading data."""
+
+    def __init__(self, state: TradingState, symbol: Symbol):
+        self.symbol = symbol
+        self.orders = []
+        self.buy_orders = state.order_depths[symbol].buy_orders.copy()
+        self.sell_orders = state.order_depths[symbol].sell_orders.copy()
+        self.position = state.position.get(symbol, 0)
+        self.position_max = POSITION_MAX[symbol]
+        self.theo = self.get_theo()
+
+    def place_order(self, price: int, volume: int):
+        # Place an order, updating the order books and position accordingly.
+        if volume == 0: return
+        self.orders.append(Order(self.symbol, price, volume))
+        if volume > 0:
+            if price in self.sell_orders:
+                self.sell_orders[price] += volume
+                if self.sell_orders[price] == 0:
+                    del self.sell_orders[price]
+            if price in self.buy_orders:
+                self.buy_orders[price] += volume
+        else: # volume < 0
+            if price in self.buy_orders:
+                self.buy_orders[price] += volume
+                if self.buy_orders[price] == 0:
+                    del self.buy_orders[price]
+            if price in self.sell_orders:
+                self.sell_orders[price] += volume
+        self.position += volume
+
+    def take_favorable_trades(self):
+        # Take all favorable trades for the product, regardless of position limits.
+        for bid in self.buy_orders.copy():
+            if bid > self.theo + EDGE:
+                trade_vol = -min(self.buy_orders[bid], self.position + self.position_max)
+                self.place_order(bid, trade_vol)
+        for ask in self.sell_orders.copy():
+            if ask < self.theo - EDGE:
+                trade_vol = min(-self.sell_orders[ask], self.position_max - self.position)
+                self.place_order(ask, trade_vol)
+
+    def reduce_absolute_position(self):
+        # Bring position closer to zero by making trades at the theo.
+        if self.position > 0:
+            for bid in self.buy_orders.copy():
+                if self.theo - 1 < bid <= self.theo:
+                    trade_vol = -min(self.buy_orders[bid], self.position)
+                    self.place_order(bid, trade_vol)
+        elif self.position < 0:
+            for ask in self.sell_orders.copy():
+                if self.theo <= ask < self.theo + 1:
+                    trade_vol = min(-self.sell_orders[ask], -self.position)
+                    self.place_order(ask, trade_vol)
+
+    def make_a_market(self):
+        # Make a market by improving orders on both sides of the theo.
+        market_bid = next((bid for bid in self.buy_orders if bid + 1 < self.theo), None)
+        market_bid_vol = self.buy_orders[market_bid] if market_bid is not None else 0
+        self.place_order(market_bid + 1, market_bid_vol)
+
+        market_ask = next((ask for ask in self.sell_orders if ask - 1 > self.theo), None)
+        market_ask_vol = self.sell_orders[market_ask] if market_ask is not None else 0
+        self.place_order(market_ask - 1, market_ask_vol)
+
+    def get_theo(self):
+        match self.symbol:
+            case 'TOMATOES':
+                return Market.get_bottom_2_levels_mean(self) - SKEW_MAX * (self.position / self.position_max)
+            case 'EMERALDS':
+                return THEO_EMERALDS
+            
+    def get_skew(self):
+        return
+
+class Market:
+    """Utility class for market price functions."""
+
+    @staticmethod
+    def get_bottom_2_levels_mean(product: Product) -> float:
+        # Heuristic for market price of tomatoes and emeralds.
+        return mean(list(product.buy_orders)[-2:] + list(product.sell_orders)[-2:])
+        
 class Trader:
 
     def run(self, state: TradingState):
-        products = ["TOMATOES", "EMERALDS"]
+        symbols = ('TOMATOES', 'EMERALDS')
         result = {}
-        for product in products:
+        for symbol in symbols:    
+            product = Product(state, symbol)
 
-            orders: List[Order] = []
+            product.take_favorable_trades()
+            product.reduce_absolute_position()
+            product.make_a_market()
     
-            if product in state.order_depths:
-                order_depth: OrderDepth = state.order_depths[product]
-                fair_value = (sum(list(order_depth.buy_orders.keys())[-2:])+sum(list(order_depth.sell_orders.keys())[-2:]))/4
-                print(fair_value)
-    
-                
-                position = state.position.get(product, 0)
-    
-                if position>0:
-                    if len(order_depth.buy_orders) != 0:
-                        best_bid = max(order_depth.buy_orders.keys())
-                        best_bid_vol = order_depth.buy_orders[best_bid]
-                        if best_bid == fair_value:
-                            orders.append(Order(product, best_bid, -min(best_bid_vol, position)))
-        
-                if position<0:
-                    if len(order_depth.buy_orders) != 0:
-                        best_ask = min(order_depth.sell_orders.keys())
-                        best_ask_vol = order_depth.sell_orders[best_ask]
-                        if best_ask == fair_value:
-                            orders.append(Order(product, best_ask, -max(best_ask_vol, position)))
-                        
-        
-                        
-               # Best bid: highest buy price (buy_orders is dict {price: volume}, volume positive)
-                    
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_vol = order_depth.buy_orders[best_bid]
-                    market_bid = list(order_depth.buy_orders.keys())[-2]
-                    market_bid_vol = order_depth.buy_orders[market_bid]
-                    if best_bid > fair_value:
-                        orders.append(Order(product, best_bid, -best_bid_vol))
-                    # Improve bid by 1 tick, same volume (positive = buy)
-                    if (market_bid + 1) < fair_value:
-                        orders.append(Order(product, market_bid + 1, market_bid_vol))
-                        
-                # Best ask: lowest sell price (sell_orders is dict {price: volume}, volume negative)
-                if len(order_depth.sell_orders) != 0:
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_vol = order_depth.sell_orders[best_ask]
-                    market_ask = list(order_depth.sell_orders.keys())[-2]
-                    market_ask_vol = order_depth.sell_orders[market_ask]
-                    if best_ask < fair_value:
-                        orders.append(Order(product, best_ask, -best_ask_vol))
-                    # Improve ask by 1 tick, same volume (negative = sell)
-                    if (market_ask - 1) > fair_value:
-                        orders.append(Order(product, market_ask - 1, market_ask_vol))
-                print(orders)
-             
-    
-            result[product] = orders
+            result[symbol] = product.orders
 
-        traderData = ""
+        traderData = ''
         conversions = 0
         return result, conversions, traderData
